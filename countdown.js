afd3a6f63beef0b3ae7c004924e7eb6578094864
Raziel <razzziel001@gmail.com>
2019-06-24 12:46:30 -0500

    No funciona por el console.log buscar error

--- countdown.js
+++ countdown.js
@@ -1,11 +1,21 @@
 //obtiene el tiempo faltante
 const getRemainTime = deadline => {
 let now = Date(),
-    remainTime = (new Date(deadline) - now+1000)/1000,
+    remainTime = (new Date(deadline) - now + 1000)/1000,
     remainSeconds = ('0'+ Math.floor(remainTime % 60)).slice(-2),
-    remainMinutes = ('0'+ Math.floor(remainTime / 60 % 60)).slice(-2)
-    remainHours = ('0'+ Math.floor(remainTime / 3600 % 24)).slice(-2)
+    remainMinutes = ('0'+ Math.floor(remainTime / 60 % 60)).slice(-2),
+    remainHours = ('0'+ Math.floor(remainTime / 3600 % 24)).slice(-2),
     remainDays = Math.floor(remainTime / (3600 * 24));
-    
+
+return {
+remainTime,
+remainSeconds,
+remainMinutes,
+remainHours,
+remainDays
+
+    }
 
 };
+
+console.log(getRemainTime('Jun 27 2019 12:31:48 GMT-0500'));
