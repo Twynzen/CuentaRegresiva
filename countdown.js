723e2fa58d739df8ec9cb5d70aad138ab09a0df6
Raziel <razzziel001@gmail.com>
2019-06-25 13:38:19 -0500

    Me faltaba declarar el new de la funcion Date

--- countdown.js
+++ countdown.js
@@ -1,6 +1,6 @@
 //obtiene el tiempo faltante
 const getRemainTime = deadline => {
-let now = Date(),
+let now = new Date(),
     remainTime = (new Date(deadline) - now + 1000)/1000,
     remainSeconds = ('0'+ Math.floor(remainTime % 60)).slice(-2),
     remainMinutes = ('0'+ Math.floor(remainTime / 60 % 60)).slice(-2),
