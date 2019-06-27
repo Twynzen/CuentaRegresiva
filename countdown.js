37ed1ce90e675393b411791932383dc0ded75b32
Raziel <razzziel001@gmail.com>
2019-06-26 22:38:23 -0500

    Finaliza el codigo de cuenta regresiva

--- countdown.js
+++ countdown.js
@@ -18,4 +18,21 @@ remainDays
 
 };
 
-console.log(getRemainTime('Jun 27 2019 12:31:48 GMT-0500'));
+const cuenta_atras = (deadline, elem, finalMessage) =>{
+  const el = document.getElementById(elem);
+
+  const timerUpdate = setInterval( () =>{
+
+    let t = getRemainTime(deadline);
+    el.innerHTML = `${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
+
+if(t.remainTime <= 1){
+  clearInterval(timerUpdate);
+  el.innerHTML = finalMessage;
+}
+
+  }, 1000)
+}
+
+cuenta_atras('Jun 26 2019 22:36:48 GMT-0500', 'clock', 'Pues ya se cumplio');
+console.log(getRemainTime('Jun 26 2019 22:36:48 GMT-0500'));
